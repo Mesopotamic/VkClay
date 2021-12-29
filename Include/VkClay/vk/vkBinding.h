8a83ba006e08bc2236c98c06016af981f55596c4
Lawrence <Lawrence.martin.green@gmail.com>
2021-12-29 17:51:54 +0000

    Add stubs for testing create instance

---
+++ Include/VkClay/vk/vkBinding.h
@@ -0,0 +1,17 @@
+// This was auto generated as part of the Vulkan bindings, so we can dynamically load Vulkan
+#ifndef __VK_CLAY_BINDINGS_H__
+#define __VK_CLAY_BINDINGS_H__
+#define VK_NO_PROTOTYPES
+#include "Vulkan_Raw/vulkan.h"
+
+/**
+ * @brief vkCreateInstance
+ * @param pCreateInfo Pointer to VkInstanceCreateInfo
+ * @param pAllocator Pointer to VkAllocationCallbacks
+ * @param pInstance Pointer to VkInstance
+ * @returns VkResult, Success codes : VK_SUCCESS, Error Codes : VK_ERROR_OUT_OF_HOST_MEMORY, VK_ERROR_OUT_OF_DEVICE_MEMORY, VK_ERROR_INITIALIZATION_FAILED, VK_ERROR_LAYER_NOT_PRESENT, VK_ERROR_EXTENSION_NOT_PRESENT, VK_ERROR_INCOMPATIBLE_DRIVER
+ */
+extern PFN_vkCreateInstance vkCreateInstance;
+
+
+#endif
