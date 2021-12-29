ead35a122ade89307137181323ccacff66dd4baf
Lawrence <Lawrence.martin.green@gmail.com>
2021-12-29 18:55:27 +0000

    Copy first implementation of a stub function

---
+++ Source/VkBindings/VkStubs.c
@@ -0,0 +1,11 @@
+// Auto generated as part of the Vulkan bindings, provides stubs
+#include "VkClay/vk/vkBinding.h"
+#include <stdio.h>
+
+VKAPI_ATTR VkResult VKAPI_CALL stub_vkCreateInstance( const VkInstanceCreateInfo* pCreateInfo, const VkAllocationCallbacks* pAllocator, VkInstance* pInstance)
+{
+	printf("Not connected vkCreateInstance to a function pointer");
+	return VK_SUCCESS;
+}
+PFN_vkCreateInstance vkCreateInstance = stub_vkCreateInstance; 
+
